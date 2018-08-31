9dfad316455dedb29e7db067ac8010a7fe8751f7
Jonathon Lui <jonathon.lui@gmail.com>
2018-08-31 16:42:28 -0700

    Realtime loading of lunches

--- lunch-web/src/App.js
+++ lunch-web/src/App.js
@@ -13,8 +13,10 @@ class App extends Component {
 
   async componentDidMount() {
     this.setState({ isLoading: true });
-    const { docs } = await db.collection('lunches').get();
-    this.setState({ isLoading: false, lunches: docs })
+    db.collection('lunches').onSnapshot(({ docs }) => {
+      const lunches = docs.map(doc => ({ id: doc.id, ...doc.data() }));
+      this.setState({ isLoading: false, lunches });
+    });
   }
 
   render() {
@@ -22,7 +24,13 @@ class App extends Component {
     return (
       <div className="App">
         <ul>
-          {lunches.map(({ id, name }) => <li key={id}>{name || id}</li>)}
+          {
+            lunches.length < 1
+              ? !isLoading && <li>No lunches</li>
+              : lunches.map(({ id, name }) => (
+                <li key={id}>{name || id}</li>
+              ))
+          }
           {isLoading ? <li>Loading...</li> : null}
         </ul>
       </div>
