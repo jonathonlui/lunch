1cbf3ee783303b20316918f7a22b6a50c1404dc8
Jonathon Lui <jonathon.lui@gmail.com>
2018-09-16 14:11:37 -0700

    Use Material UI

--- lunch-web/src/App.js
+++ lunch-web/src/App.js
@@ -1,8 +1,31 @@
 import React, { Component } from 'react';
-import './App.css';
+
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import RefreshIcon from '@material-ui/icons/Refresh';
+
+
 import { db } from './firebase';
 
 import LunchList from './components/LunchList';
+
+
+const styles = theme => ({
+  App: {
+    margin: '0 auto',
+    padding: 2 * theme.spacing.unit,
+    maxWidth: 600,
+    fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
+    fontSize: '0.875rem',
+    color: '#212121',
+  },
+  leftIcon: {
+    marginRight: theme.spacing.unit,
+  },
+});
 
 
 class App extends Component {
@@ -29,23 +52,33 @@ class App extends Component {
   };
 
   render() {
+    const { classes } = this.props;
     const { isLoading, lunches } = this.state;
     return (
-      <div className="App">
-        <h1>Lunch in DTLA</h1>
-        <LunchList lunches={lunches} isLoading={isLoading} />
-        <div className="AppFooter">
-          <button
-            type="button"
-            disabled={isLoading}
-            onClick={this.reloadLunches}
-          >
-            Refresh
-          </button>
+      <React.Fragment>
+        <CssBaseline />
+        <div className={classes.App}>
+          <Typography variant="display3" gutterBottom>
+            Lunch
+            <Button disabled={isLoading} onClick={this.reloadLunches}>
+              {isLoading
+                ? <CircularProgress size={24} />
+                : (
+                  <React.Fragment>
+                    <RefreshIcon className={classes.leftIcon} />
+                    Refresh
+                  </React.Fragment>
+                )
+              }
+            </Button>
+          </Typography>
+
+          <LunchList lunches={lunches} isLoading={isLoading} />
+
         </div>
-      </div>
+      </React.Fragment>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
--- lunch-web/src/components/LunchList/index.js
+++ lunch-web/src/components/LunchList/index.js
@@ -1,5 +1,37 @@
 import React from 'react';
+
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+
+import placeholderImage4x3 from '../../assets/placeholder4x3.png';
+
 import './index.css';
+
+
+const styles = theme => ({
+  Card: {
+    marginBottom: 4 * theme.spacing.unit,
+  },
+  Description: {
+    marginBottom: 1.5 * theme.spacing.unit,
+  },
+  image4x3: {
+    height: 0,
+    paddingTop: '75%',
+  },
+  // image16x9: {
+  //   height: 0,
+  //   paddingTop: '56.25%',
+  // },
+  // image1x1: {
+  //   height: 0,
+  //   paddingTop: '100%',
+  // },
+});
 
 
 function toCurrency(value, defaultValue, { locale = 'en-US', currency = 'USD' } = {}) {
@@ -28,26 +60,17 @@ const MealList = ({ meals }) => (
 );
 
 
-const LunchItemPriceRange = ({ meals }) => {
-  if (!meals || meals.length < 1) {
-    return null;
-  }
+const getPriceRangeString = (meals) => {
   const prices = meals.map(({ price }) => price).sort((a, b) => a - b);
   const low = prices[0];
   const high = prices.slice(-1)[0];
-  const rangeString = low === high
+  return low === high
     ? toCurrency(low)
     : `${toCurrency(low)} - ${toCurrency(high)}`;
-
-  return (
-    <div className="LunchItemPriceRange">
-      {rangeString}
-    </div>
-  );
 };
 
 
-const LunchItem = ({
+const LunchCard = withStyles(styles)(({
   lunch: {
     id,
     name,
@@ -55,38 +78,41 @@ const LunchItem = ({
     meals,
     address,
     yelpLink,
+    imageUrl = placeholderImage4x3,
   },
+  classes,
 }) => (
-  <li className="LunchItem">
-    <div className="LunchItemName">{name || id}</div>
-    <LunchItemPriceRange meals={meals} />
-    {description && <div className="LunchItemDescription">{description}</div>}
-    <MealList meals={meals} />
-    <div className="LunchItemFooter">
-      {address && <div className="LunchItemFooterAddress">{address}</div>}
-      <div className="LunchItemFooterLinks">
-        {address && (
-          <span>
-            <a href={`https://www.google.com/maps/dir/?api=1&travelmode=walking&destination=${encodeURIComponent(address)}`}>Google Maps</a>
-            {' '}
-            <a href={`https://maps.apple.com/?dirflg=w&daddr=${encodeURIComponent(address)}`}>Apple Maps</a>
-          </span>
-        )}
-        {' '}
-        {yelpLink && <a href={yelpLink}>Yelp</a>}
+  <Card component="li" className={classes.Card}>
+    <CardHeader title={name || id} subheader={getPriceRangeString(meals)} />
+    <CardMedia className={classes.image4x3} image={imageUrl} />
+    <CardContent>
+      {description && (
+        <Typography variant="body1" className={classes.Description}>
+          {description}
+        </Typography>
+      )}
+      <MealList meals={meals} />
+      <div className="LunchItemFooter">
+        {address && <div className="LunchItemFooterAddress">{address}</div>}
+        <div className="LunchItemFooterLinks">
+          {address && (
+            <span>
+              <a href={`https://www.google.com/maps/dir/?api=1&travelmode=walking&destination=${encodeURIComponent(address)}`}>Google Maps</a>
+              {' '}
+              <a href={`https://maps.apple.com/?dirflg=w&daddr=${encodeURIComponent(address)}`}>Apple Maps</a>
+            </span>
+          )}
+          {' '}
+          {yelpLink && <a href={yelpLink}>Yelp</a>}
+        </div>
       </div>
-    </div>
-  </li>
-);
+    </CardContent>
+  </Card>
+));
 
 
 const NoLunches = () => (
   <li className="LunchItem">No lunches</li>
-);
-
-
-const LunchesLoading = () => (
-  <li className="LunchItem">Loading lunches...</li>
 );
 
 
@@ -95,11 +121,10 @@ const LunchList = ({ lunches, isLoading }) => (
     {
       lunches.length < 1 && !isLoading
         ? <NoLunches />
-        : lunches.map(lunch => <LunchItem key={lunch.id} lunch={lunch} />)
+        : lunches.map(lunch => <LunchCard key={lunch.id} lunch={lunch} />)
     }
-    {isLoading && <LunchesLoading />}
   </ul>
 );
 
 
-export default LunchList;
+export default withStyles(styles)(LunchList);
