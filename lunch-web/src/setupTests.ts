838e4f2deb9b7f66433e7fa3bd8ddf4a98cd1641
Jonathon Lui <jonathon.lui@gmail.com>
2019-03-30 14:39:07 -0700

    Disable lint in src/setupTests.ts

--- lunch-web/src/setupTests.ts
+++ lunch-web/src/setupTests.ts
@@ -1,3 +1,5 @@
+/* eslint-disable import/no-extraneous-dependencies */
+
 // react-testing-library renders your components to document.body,
 // this will ensure they're removed after each test.
 import 'react-testing-library/cleanup-after-each';
